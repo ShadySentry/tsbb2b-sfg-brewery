fe8c01d991262428121f6cf7c963981c82f238e9
John Thompson <john@springframework.guru>
2019-03-02 14:22:53 -0500

    initial project add

--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -1,9 +1,7 @@
 package guru.springframework.brewery;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 public class TsbbSfgBreweryApplicationTests {
