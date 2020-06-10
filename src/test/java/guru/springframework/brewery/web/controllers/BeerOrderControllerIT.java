ecae0fc5ae707a214e79102e0559e33dcc4d347d
ShadySentry <eurohawk@gmail.com>
2020-06-10 16:27:35 +0300

    BeerOrderControllerIT created

--- src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerIT.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerIT.java
@@ -1,19 +1,37 @@
 package guru.springframework.brewery.web.controllers;
 
+import guru.springframework.brewery.domain.Customer;
+import guru.springframework.brewery.repositories.CustomerRepository;
 import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@WebMvcTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BeerOrderControllerIT {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    CustomerRepository customerRepository;
+
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = customerRepository.findAll().get(0);
+    }
+
     @Test
     void testListOrders() {
-        BeerPagedList pagedList = testRestTemplate.getForObject("")
+        BeerPagedList pagedList = testRestTemplate.getForObject("/api/v1/customers/" +
+                customer.getId() + "/orders",BeerPagedList.class);
+        assertThat(pagedList.getContent()).hasSize(1);
     }
 }
